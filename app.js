d78a8e41471d4f6ef0d6e26e4cede03fff3087f9
urbannomad94 <95559192+urbannomad94@users.noreply.github.com>
2022-01-19 12:52:25 -0500

    Used JS to make app interactive

--- app.js
+++ app.js
@@ -1,89 +1,120 @@
-//Computer will randomly choose rock, paper, or scissors
-function computerPlay() {
-    random = Math.random()*3;
-    if (random <= 1) {
-        return "rock";
-    }
-    else if (random <= 2) {
-        return "paper";
-    }
-    else {
-        return "scissors";
-    }
-}
-
-//Single round of RPS
-function playRound (playerSelection, computerSelection) {
-    playerSelection = prompt("Rock, paper, or scissors?").toLowerCase();
-    computerSelection = computerPlay();
-
-    console.log("You selected:", playerSelection);
-    console.log("The computer selected:", computerSelection);
-
-    if (playerSelection === computerSelection) {
-        alert("Tie");
-        console.log("Result: Tie");
-        tie++;
-    }
-
-    else if (playerSelection === "rock" && computerSelection === "paper"){
-        alert ("You lose, paper beats rock.");
-        console.log ("Result: You lose, paper beats rock.");
-        loss++;
-    }
-
-    else if (playerSelection === "rock" && computerSelection === "scissors"){
-        alert ("You win, rock beats scissors.");
-        console.log ("Result: You win, rock beats scissors.");
-        win++;
-    }
-
-    else if (playerSelection === "paper" && computerSelection === "rock"){
-        alert ("You win, paper beats rock.");
-        console.log ("Result: You win, paper beats rock.");
-        win++;
-    }
-
-    else if (playerSelection === "paper" && computerSelection === "scissors"){
-        alert ("You lose, scissors beats paper.");
-        console.log ("Result: You lose, scissors beats paper.");
-        loss++;
-    }
-
-    else if (playerSelection === "scissors" && computerSelection === "rock"){
-        alert ("You lose, rock beats scissors.");
-        console.log ("Result: You lose, rock beats scissors.");
-        loss++;
-    }
-
-    else if (playerSelection === "scissors" && computerSelection === "paper"){
-        alert ("You win, scissors beats paper.");
-        console.log  ("Result: You win, scissors beats paper.");
-        win++;
-    }
-
-    else {
-        alert("Not a valid entry.")
-        console.log  ("Not a valid entry");
-    }
-
-}
-
-//5 rounds of RPS and alerts wins, losses, and ties after last round
-function game() {
-    for (let i = 0; i < 5; i++) {
-        playRound(playerSelection, computerSelection);
-    }
-    alert("Wins: " + win + "\nLosses: " + loss + "\nTies: " + tie);
-}
-
 //Declaring variables
-let computerSelection;
+const computerSelectionDisplay = document.getElementById('computer-selection');
+const playerSelectionDisplay = document.getElementById('player-selection');
+const resultDisplay = document.getElementById('result');
+const scoreDisplay = document.getElementById('score');
+const possibleChoices = document.querySelectorAll('.buttons');
+const reset = document.getElementById('reset');
 let playerSelection;
+let computerSelection;
+let result;
+let score;
 
 let tie = 0;
 let win = 0;
 let loss = 0;
+let compWin = 0;
+let compLoss = 0;
 
-//Running game function
-game();
+//Assigns user's choice based on which button they click
+possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('click', (e) => {
+   playerSelection = e.target.id;
+   playerSelectionDisplay.innerHTML = playerSelection;
+   computerPlay();
+   playRound();
+   scoreCalculation();
+   displayWinner();
+}))
+
+//Computer will randomly choose rock, paper, or scissors
+function computerPlay() {
+    random = Math.random()*3;
+    if (random <= 1) {
+        computerSelection = "rock";
+    }
+    else if (random <= 2) {
+        computerSelection = "paper";
+    }
+    else {
+        computerSelection = "scissors";
+    }
+    computerSelectionDisplay.innerHTML = computerSelection;
+}
+
+//Single round of RPS
+function playRound () {
+    console.log("You selected:", playerSelection);
+    console.log("The computer selected:", computerSelection);
+
+    if (playerSelection === computerSelection) {
+        result = "Tie";
+        tie++;
+    }
+
+    else if (playerSelection === "rock" && computerSelection === "paper"){
+        result = "You lose, paper beats rock.";
+        loss++;
+        compWin++;
+    }
+
+    else if (playerSelection === "rock" && computerSelection === "scissors"){
+        result = "You win, rock beats scissors.";
+        win++;
+        compLoss++;
+    }
+
+    else if (playerSelection === "paper" && computerSelection === "rock"){
+        result = "You win, paper beats rock.";
+        win++;
+        compLoss++;
+    }
+
+    else if (playerSelection === "paper" && computerSelection === "scissors"){
+        result = "You lose, scissors beats paper.";
+        loss++;
+        compWin++;
+    }
+
+    else if (playerSelection === "scissors" && computerSelection === "rock"){
+        result = "You lose, rock beats scissors.";
+        loss++;
+        compWin++;
+    }
+
+    else if (playerSelection === "scissors" && computerSelection === "paper"){
+        result = "You win, scissors beats paper.";
+        win++;
+        compLoss++;
+    }
+
+    else {
+        result = "Not a valid entry.";
+    }
+    resultDisplay.innerHTML = result;
+}
+
+//Displays the current score (win-loss-tie)
+function scoreCalculation() {
+    score = `${win} - ${compWin} - ${tie}`;
+    scoreDisplay.innerHTML = score;
+}
+
+//Alerts the winner once the user of computer reaches 5 wins
+function displayWinner() {
+    if (win >= 5) {
+        alert("You won!");
+    }
+    if (compWin >= 5) {
+        alert("You lost!");
+    }
+}
+
+//Gives reset button functionality
+reset.addEventListener ('click', () => {
+    tie = 0;
+    win = 0;
+    loss = 0;
+    compWin = 0;
+    compLoss = 0;
+    location.reload();
+})
